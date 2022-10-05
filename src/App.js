9b7309dd36501aa24194a08ee01f00b762b9fa79
Mayu Hayakawa <mayu.i.19930327@gmail>
2022-10-04 19:40:24 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Component } from 'react'; //クラスコンポーネントのファイル参照
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+//クラスコンポーネントの拡張
+class App extends Component {
+  constructor() {
+    super();
+
+    // https://pokeapi.co/api/v2/pokemon/810
+    this.state = {
+      pokemons: []
+    };
+  }
+  //コンポーネントがDOM上に配置された瞬間が、APIリクエストを行うタイミング
+  componentDidMount() {
+    for(let i = 810; i < 898; i++){
+      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      // fetch(`https://pokeapi.co/api/v2/pokemon/810`)
+      .then((response) => response.json())
+      .then((datas) => //console.log(datas)
+        this.setState(
+          () => {
+            return { pokemons: datas};
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          {this.state.pokemons.map((pokemon) => {
+              return (
+              <div key={pokemon.id}>
+                <h1>{pokemon.name}</h1>
+              </div>
+              )
+            })
+          }
+      </div>
+    );
+  };
+};
 
 export default App;
