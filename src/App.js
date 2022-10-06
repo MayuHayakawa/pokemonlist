1fea3f0918a252b4e973f1bee4cfbb2addf6dde4
Mayu Hayakawa <mayu.i.19930327@gmail>
2022-10-05 19:36:43 -0700

    checking how to get each pokemon's value

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
-import { Component } from 'react'; //クラスコンポーネントのファイル参照
+//File references for class components
+import { Component } from 'react';
 
 import './App.css';
 
 
-//クラスコンポーネントの拡張
+//Extending Class Components
 class App extends Component {
   constructor() {
     super();
 
     // https://pokeapi.co/api/v2/pokemon/810
     this.state = {
-      pokemons: []
+      pokemons: [],
+      
     };
   }
   //コンポーネントがDOM上に配置された瞬間が、APIリクエストを行うタイミング
   componentDidMount() {
-    for(let i = 810; i < 898; i++){
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      // fetch(`https://pokeapi.co/api/v2/pokemon/810`)
-      .then((response) => response.json())
-      .then((datas) => //console.log(datas)
-        this.setState(
-          () => {
-            return { pokemons: datas};
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
+    this.getPokemons();
+  }
+
+  getPokemons(){
+    // gets all pokemons
+    fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    // fetches the pokemons from the api
+    .then(
+      // then we get the response and convert it to json
+      (response) => response.json()
+    ).then((data) => {
+      // then we set the state to the data we got from the api
+      this.setState(
+        () => {
+          return { pokemons: data.results};
+        }
       )
-    }
+    })
+  }
+
+  getPokemonsById(id){
+    // gets pokemon by id
+    // you can use this when you click on a pokemon to get more details.
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(
+      (response) => response.json()
+    ).then((data) => {
+      this.setState(
+        () => {
+          return { data }
+          // console.log(data);
+        }
+      )
+    })
   }
 
   render() {
     return (
       <div className="App">
-          {this.state.pokemons.map((pokemon) => {
+          {this.state.pokemons.map((pokemon, index) => {
               return (
-              <div key={pokemon.id}>
+              <div key={index}>
                 <h1>{pokemon.name}</h1>
+
+                {/* How can I get img src...?
+                (Now I get pokemon's details with pokemon's name not id, because I don't know how to get id data from pokemons'state.)
+                Should I create new state of each pokemons api data to get value like img?(I mean it is array's of `https://pokeapi.co/api/v2/pokemon/${id}`)
+                Or it's better to get id to get img's src?(ex; `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png`) */}
+                <img src="{this.getPokemonsById(pokemon.name).sprites.front_default}" />
+                {/* <button onClick={this.getPokemonsById(pokemon.name)}>Detail</button> */} 
               </div>
               )
             })
