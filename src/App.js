fdac965f1a1aad1453d3130fa793744ed143790d
Mayu Hayakawa <mayu.i.19930327@gmail>
2022-10-10 21:58:41 -0700

    add img(doesn't work) and searchbox

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     // https://pokeapi.co/api/v2/pokemon/810
     this.state = {
       pokemons: [],
-      
+      searchField: '',
     };
   }
-  //コンポーネントがDOM上に配置された瞬間が、APIリクエストを行うタイミング
+  //The moment a component is placed on the DOM is the time to make an API request
   componentDidMount() {
     this.getPokemons();
   }
@@ -23,6 +23,8 @@ class App extends Component {
   getPokemons(){
     // gets all pokemons
     fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    // fetch(`https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0`)
+
     // fetches the pokemons from the api
     .then(
       // then we get the response and convert it to json
@@ -46,27 +48,60 @@ class App extends Component {
     ).then((data) => {
       this.setState(
         () => {
-          return { data }
-          // console.log(data);
+          console.log(data);
+        }
+      )
+    })
+  }
+
+  getPokemonsImgByName(name){
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(
+      (response) => response.json()
+    ).then((data) => {
+      this.setState(
+        () => {
+          //ex; https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/722.png
+          return data.sprites.front_default;
+          // console.log(data.sprites.front_default);
         }
       )
     })
   }
 
   render() {
+    //Update values of pokemons(this state) each time you input something to searchbox 
+    const filteredPokemons = this.state.pokemons.filter((pokemon) => {
+      return pokemon.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+    
     return (
-      <div className="App">
-          {this.state.pokemons.map((pokemon, index) => {
-              return (
+      <div className='App'>
+        <input 
+          className='search-box'
+          type='search'
+          placeholder='search pokemons' 
+          //when you input something to this searchbox, searchField state is created
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            })
+          }}
+          />
+          {filteredPokemons.map((pokemon, index) => {
+            return (
               <div key={index}>
                 <h1>{pokemon.name}</h1>
 
                 {/* How can I get img src...?
-                (Now I get pokemon's details with pokemon's name not id, because I don't know how to get id data from pokemons'state.)
-                Should I create new state of each pokemons api data to get value like img?(I mean it is array's of `https://pokeapi.co/api/v2/pokemon/${id}`)
-                Or it's better to get id to get img's src?(ex; `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{id}.png`) */}
-                <img src="{this.getPokemonsById(pokemon.name).sprites.front_default}" />
-                {/* <button onClick={this.getPokemonsById(pokemon.name)}>Detail</button> */} 
+                (Now I get pokemon's details with pokemon's name not id (getPokemonsImgByName()),
+                because I don't know how to get id data from pokemons'state...) */}
+                <img src="{this.getPokemonsImgByName(pokemon.name)}" alt={pokemon.name}/>
+
+                {/* ↓It can get img's url on console when I set getPokemonsImgByName()'s setState is console.log(), but I don't know why...
+                <button onClick={this.getPokemonsImgByName(pokemon.name)}></button> */}
+
               </div>
               )
             })
